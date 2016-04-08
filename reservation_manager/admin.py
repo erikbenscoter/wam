e9e530e24135c383a78e041fbc5f4b9b76f66446
photoserver <photoserver@photoserver.(none)>
2016-04-08 19:19:49 -0400

    initial commit

---
+++ reservation_manager/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Owner
+from .models import Reservation
+
+# Register your models here.
+admin.site.register( Owner )
+admin.site.register( Reservation )
