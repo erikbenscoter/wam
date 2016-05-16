b2214dce66c9308e0e9239a2b0b9f0103b76837d
erik benscoter <e.benscoter@gmail.com>
2016-05-15 20:56:04 -0400

    remvoed commented out code

--- re-migrate.sh
+++ re-migrate.sh
@@ -2,22 +2,6 @@
 source /home/erikbenscoter/.bashrc
 
 python3 manage.py migrate
-#
-# echo "++++ initial makemigrations +++++"
-# python3 manage.py makemigrations
-# python3 manage.py migrate
-#
-# echo "++++ reservation_manager makemigrations +++++"
-# python3 manage.py makemigrations reservation_manager
-# python3 manage.py migrate
-#
-# echo "++++ login/monthly_summary/owners_point_manager makemigrations +++++"
-# python3 manage.py makemigrations login
-# python3 manage.py makemigrations monthly_summary
-# python3 manage.py makemigrations owners_point_manager
-#
-# echo "++++ migrate ++++"
-# python3 manage.py migrate
-#
+
 echo "++++ loading fixtures ++++"
 python3 manage.py loaddata ./fixtures/*
