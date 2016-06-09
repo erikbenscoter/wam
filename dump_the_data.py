be57319e5818ad2c1aa96e0a4c85cbd58d9679d2
erik benscoter <e.benscoter@gmail.com>
2016-06-08 20:01:46 -0400

    fixes to dump the data commented out button on index.html json updates

--- dump_the_data.py
+++ dump_the_data.py
@@ -1,5 +1,7 @@
 from VacationScheduler.settings import INSTALLED_APPS
+import os
 
 for app in INSTALLED_APPS:
-    cmd = "python3 manage.py dumpdata {} --indent=4 > ./fixtures/{}.json".format(str(app),str(app))
-    os.system(cmd)
+    if(not len(app.split(".")) > 1):
+        cmd = "python3 manage.py dumpdata {} --indent=4 > ./fixtures/{}.json".format(str(app),str(app))
+        os.system(cmd)
